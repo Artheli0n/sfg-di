8dd43d48b99031a5c589a6b2ffe98193d8421e29
boudoual <alexandre.bourdouxhe@arhs-dev-be.com>
2021-02-23 13:58:26 +0100

    Section 5 - Factory Bean : adding lots of comments to explain all steps here

--- src/main/java/spring/training/personal/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/spring/training/personal/sfgdi/config/GreetingServiceConfig.java
@@ -11,11 +11,21 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 public class GreetingServiceConfig {
 
+    // this injects GreetingRepository (it is a @Component) into GreetingServiceFactory, and the created factory is registered
+    // as a Bean thanks to the @Bean annotation of this method
     @Bean
     GreetingServiceFactory greetingServiceFactory(GreetingRepository repository) {
         return new GreetingServiceFactory(repository);
     }
 
+    // this injects GreetingServiceFactory (it is a @Bean thanks to previous method) to call its createGreetingService method
+    // and the injected GreetingRepository of the factory is injected in the GreetingService because it was injected in the
+    // factory at creation
+    // The newly created GreetingService is registered as the Primary (thanks to @Primary) Bean (thanks to @Bean) greetingService
+    // for profiles default, en
+    // Primary was necessary because of the ContructorGreetingService, GreetingServiceImpl, SetterInjectedGreetingService and
+    // PropertyInjectedGreetingService which are not limited to some profiles but also implements the GreetingService interface
+    // and are defined as Bean (through @Service)
     @Bean
     @Primary
     @Profile({"default", "en"})
@@ -23,6 +33,8 @@ public class GreetingServiceConfig {
         return greetingServiceFactory.createGreetingService("en");
     }
 
+    // The newly created GreetingService is registered as the Primary (thanks to @Primary) Bean (thanks to @Bean) greetingService
+    // for profiles es
     @Bean
     @Primary
     @Profile({"es"})
@@ -30,6 +42,8 @@ public class GreetingServiceConfig {
         return greetingServiceFactory.createGreetingService("es");
     }
 
+    // The newly created GreetingService is registered as the Primary (thanks to @Primary) Bean (thanks to @Bean) greetingService
+    // for profiles de
     @Bean
     @Primary
     @Profile({"de"})
