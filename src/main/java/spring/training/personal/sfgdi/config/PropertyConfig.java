7251a0106b60d16fb731b0bf4e2c1fcc0995dbe1
boudoual <alexandre.bourdouxhe@arhs-dev-be.com>
2021-02-24 09:54:01 +0100

    Section 6 - externalized files application.pro  : how to use properties files to add properties variables in the app with application.properties

--- src/main/java/spring/training/personal/sfgdi/config/PropertyConfig.java
+++ src/main/java/spring/training/personal/sfgdi/config/PropertyConfig.java
@@ -2,26 +2,13 @@ package spring.training.personal.sfgdi.config;
 
 import spring.training.personal.sfgdi.examplebeans.FakeDataSource;
 import spring.training.personal.sfgdi.examplebeans.FakeJmsBroker;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-import org.springframework.core.env.Environment;
 //import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
-//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties" })
-@PropertySources({
-        @PropertySource("classpath:datasource.properties"),
-        @PropertySource("classpath:jms.properties"),
-})
-// the externalized file classpath, with classpath in Maven being /src/main/resources
 public class PropertyConfig {
-
-    @Autowired
-    Environment env;
 
     // go read that value from the externalized file
     @Value("${guru.username}")
@@ -46,7 +33,7 @@ public class PropertyConfig {
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
         fakeDataSource.setUser(user);
-        fakeDataSource.setPassword(env.getProperty("PASSWORD"));
+        fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
     }
@@ -60,16 +47,4 @@ public class PropertyConfig {
         return fakeJmsBroker;
     }
 
-    // Note about the static annotation for below function:
-    // Any BeanFactoryPostProcessor beans (This includes PropertySourcesPlaceholderConfigurer) have to be declared static.
-    // This ensure that it is instantiated  early in the spring container life-cycle and before the containing @Configuration
-    // class so that it can resolve @Value annotation.
-
-    // the PropertySourcesPlaceholderConfigurer is what is going to read the externalized file for us
-    // starting with Spring5, it is actually redundant : From Official Doc
-    // Use this if you need to customize configuration such as the placeholder syntax, etc.
-//    @Bean
-//    public static PropertySourcesPlaceholderConfigurer properties() {
-//        return new PropertySourcesPlaceholderConfigurer();
-//    }
 }
